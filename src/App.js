cf26d62b478a70227a45988eaf2c3a5397a1bbb3
Thiago Cerutti <thiago.cerutti@rdstation.com>
2023-03-07 22:46:14 -0300

    chore: add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import MatrixCombinations from "./components/MatrixCombinations";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return <MatrixCombinations />;
--- src/components/MatrixCombinations.js
+++ src/components/MatrixCombinations.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import combineAllMatrixElements from "../services/combinations";
+import { Button } from "react-bootstrap";
 
 const MatrixCombinations = () => {
   const [firstContent, setFirstContent] = useState("a\nb\nc");
@@ -7,26 +8,17 @@ const MatrixCombinations = () => {
   const [thirdContent, setThirdContent] = useState("x\ny\nz");
   const [resultCombination, setResultCombination] = useState("");
 
-  const combineAll = () => {
-    const matrix = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-    ];
-    const result = combineAllMatrixElements(matrix);
-    console.log(result);
-  };
-
   const combineTextArea = () => {
     const matric = [
       firstContent.split("\n"),
       secondContent.split("\n"),
       thirdContent.split("\n"),
     ];
-    const x = combineAllMatrixElements(matric);
-    const y = x.map((item) => item.join(" ")).join('\n');
-    setResultCombination(y);
-    console.log(y);
+    const combinedElements = combineAllMatrixElements(matric);
+    const formattedOutput = combinedElements
+      .map((item) => item.join(" "))
+      .join("\n");
+    setResultCombination(formattedOutput);
   };
 
   const copyResultContent = (content) => {
@@ -34,37 +26,43 @@ const MatrixCombinations = () => {
   };
 
   return (
-    <>
+    <div className="container">
       <h1>Matrix combinations</h1>
-      <div>
-        <textarea
-          cols={30}
-          rows={10}
-          onChange={(event) => setFirstContent(event.target.value)}
-          value={firstContent}
-        ></textarea>
+      <div className="row">
+        <div className="col">
+          <textarea
+            className="form-control"
+            cols={30}
+            rows={10}
+            onChange={(event) => setFirstContent(event.target.value)}
+            value={firstContent}
+          ></textarea>
+        </div>
+        <div className="col">
+          <textarea
+            className="form-control"
+            cols={30}
+            rows={10}
+            onChange={(event) => setSecondContent(event.target.value)}
+            value={secondContent}
+          ></textarea>
+        </div>
+        <div className="col">
+          <textarea
+            className="form-control"
+            cols={30}
+            rows={10}
+            onChange={(event) => setThirdContent(event.target.value)}
+            value={thirdContent}
+          ></textarea>
+        </div>
       </div>
-      <div>
-        <textarea
-          cols={30}
-          rows={10}
-          onChange={(event) => setSecondContent(event.target.value)}
-          value={secondContent}
-        ></textarea>
+
+      <div className="row">
+        <Button onClick={combineTextArea}>Combine inputs</Button>
       </div>
-      <div>
-        <textarea
-          cols={30}
-          rows={10}
-          onChange={(event) => setThirdContent(event.target.value)}
-          value={thirdContent}
-        ></textarea>
-      </div>
-      <div>
-        <button onClick={combineAll}>Combine it all!</button>
-        <button onClick={combineTextArea}>combine textarea</button>
-      </div>
-      <div>
+
+      <div className="row">
         <h1>Result combination</h1>
         <textarea
           cols={30}
@@ -72,9 +70,11 @@ const MatrixCombinations = () => {
           value={resultCombination}
           readOnly
         ></textarea>
-        <button onClick={() => copyResultContent(resultCombination)}>Copy content</button>
+        <Button onClick={() => copyResultContent(resultCombination)}>
+          Copy content
+        </Button>
       </div>
-    </>
+    </div>
   );
 };
 
